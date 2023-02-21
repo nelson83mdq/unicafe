ee6c17f12a327feba3935737d9e0855e7d2ef89d
Nelson Damian Brizuela <nelson83mdq@gmail.com>
2023-02-21 17:11:32 -0300

    Unicafe paso 2

--- src/App.js
+++ src/App.js
@@ -8,25 +8,36 @@ function App() {
 
   const handleGood = () =>{
     setGood(good+1);
-    console.log('good', good);
   };
   const handleNeutral = () =>{
     setNeutral(neutral+1);
-    console.log('Neutral', neutral);
   };
   const handleBad = () =>{
     setBad(bad+1);
-    console.log('Bad', bad);
   };
-
+  const all = bad + neutral + good;
+  const average = (goodVotes, badVotes, neutralVotes)=> {
+    let sum = goodVotes - badVotes ;
+    return sum/(goodVotes + neutralVotes + badVotes);
+  }
 
   return (
     <div className="App">
-      <button onClick={handleGood}>Good</button>
-      <button onClick={handleNeutral}>Neutral</button>
-      <button onClick={handleBad}>Bad</button>
+      <h2>Give Feedback please!</h2>
+      <button className = 'feedback-button' onClick={handleGood}>Good</button>
+      <button className='feedback-button' onClick={handleNeutral}>Neutral</button>
+      <button className='feedback-button' onClick={handleBad}>Bad</button>
+      <hr/>
+      <h2>Statistics.</h2>
+      <p>Good: {good} </p>
+      <p>Neutral: {neutral} </p>
+      <p>Bad: {bad}</p>
+      <p>All: {good+ neutral + bad}</p>
+      <p>Average: {average(good, bad, neutral)}</p>
+      <p>Positive: {(good/all)*100}%</p>  
     </div>
   );
+  //<p>Positive: {(good/all)*100}%</p> muestra un NAN al comienzo debido a la falta de datos
 }
 
 export default App;
