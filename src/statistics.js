433f090c5eaecb408369ee027d0db4e5caabbece
Nelson Damian Brizuela <nelson83mdq@gmail.com>
2023-02-23 00:08:58 -0300

    Final paso5 Unicafe

--- src/statistics.js
+++ src/statistics.js
@@ -1,57 +1,36 @@
-/*
-const Statistics = (props) => {
 
-    const good = props.good ;
-    const bad = props.bad ;
-    const neutral = props.neutral;
-    const sum = good + bad+ neutral ;
-
-    const average = (goodVotes, badVotes, neutralVotes)=> {
-        let sum = goodVotes - badVotes ;
-        return sum/(goodVotes + neutralVotes + badVotes);
-      }
-    if (sum === 0){
-        return (
-            <h2>No feedback given yet, please give us feedback!</h2>
-        );
-    } else {
-    return (
-        <div className="statistical-data">
-        <p>Good: {good} </p>
-        <p>Neutral: {neutral} </p>
-        <p>Bad: {bad}</p>
-        <p>All: {sum}</p>
-        <p>Average: {average(good, bad, neutral)}</p>
-        <p>Positive: {(good/sum)*100}%</p> 
+const StatisticLine = ({name, value}) => {
+    return ( 
+        <div className='statistics-line'>
+            <p>{name}: {value}</p>
         </div>
-    );
-    }
+     );
 }
  
-export default Statistics;
+//export default StatisticLine;
 
-// componente anterior queda sin uso, ya que se genera un componente nuevo especializado mediante props.
-// the previous component has no more use, instead i generated a new component that can be specialized using props
 
-*/
-//---------------------------------------
 const Statistics = (props) => {
-    const name = props.name ;
-    //const votes = props.votes;
-    const [gVotes, nVotes, bVotes] = props.values;
+    const [gVotes, nVotes, bVotes] = props.values ;
+    const Total = gVotes + nVotes + bVotes ;
+    const average = (gVotes-bVotes)/Total;
+    const positive = (gVotes / Total)*100;
 
-    const sum = () =>{
-        return gVotes + nVotes + bVotes ;
-    };
-
-    const average = ()=> {
-        let acum = gVotes - bVotes ;
-        return acum/sum();
-      }
-
+    if ((Total) === 0){
+        return (
+            <div className='statistics-none'>
+                <h2>No feedback given yet!</h2>
+            </div>
+        );
+    }
     return (
         <div className="statistics-view">
-            <p>{name}: {}</p>
+            <StatisticLine name='Good' value={gVotes}/>
+            <StatisticLine name='Neutral' value={nVotes}/>
+            <StatisticLine name='Bad' value={bVotes}/>
+            <StatisticLine name='All' value={Total}/>
+            <StatisticLine name='Average' value={average}>%</StatisticLine>
+            <StatisticLine name='Positives' value={positive}>%</StatisticLine>
         </div>
     );
 }
